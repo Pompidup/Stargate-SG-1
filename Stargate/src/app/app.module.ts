8d9af53f1879c5261b89eed696625a1c28f09903
Pompidup <julienauvray28@gmail.com>
2019-04-10 16:58:48 +0200

    merge modif on module.ts and component.html

--- Stargate/src/app/app.module.ts
+++ Stargate/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StargateComponent } from './stargate/stargate.component';
@@ -19,7 +19,6 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     ContactUsComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
