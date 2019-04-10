c164953063a2caaeffc85f8aed8e37b88112648a
Pompidup <julienauvray28@gmail.com>
2019-04-10 14:49:57 +0200

    Ajout images, premiere intégration des components

--- Stargate/src/app/app.module.ts
+++ Stargate/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StargateComponent } from './stargate/stargate.component';
 import { HightscoreComponent } from './hightscore/hightscore.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuestComponent } from './quest/quest.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { QuestComponent } from './quest/quest.component';
     StargateComponent,
     HightscoreComponent,
     QuizComponent,
-    QuestComponent
+    QuestComponent,
+    ContactUsComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
