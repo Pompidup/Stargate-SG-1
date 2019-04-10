eae44ad1d0d46767d680136633c1ff07fa3b33f3
Pompidup <julienauvray28@gmail.com>
2019-04-10 10:38:10 +0200

    Add first component on angular

--- Stargate/src/app/app.module.ts
+++ Stargate/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StargateComponent } from './stargate/stargate.component';
+import { HightscoreComponent } from './hightscore/hightscore.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestComponent } from './quest/quest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StargateComponent,
+    HightscoreComponent,
+    QuizComponent,
+    QuestComponent
   ],
   imports: [
     BrowserModule,
