1e81a5e0953d6a19f4cb6384ecf5f11987a3f180
Yorann Elisabeth <yorannelisabeth@gmail.com>
2019-04-25 14:58:41 +0200

    gestion conflit

--- Stargate/src/app/app.module.ts
+++ Stargate/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { QuizComponent } from './quiz/quiz.component';
 import { QuestComponent } from './quest/quest.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { OnlyquizComponent } from './onlyquiz/onlyquiz.component';
+import {NasaService} from './nasa.service';
+import { PictureViewerComponent } from './picture-viewer/picture-viewer.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+ 
 
 @NgModule({
   declarations: [
@@ -19,7 +23,9 @@ import { OnlyquizComponent } from './onlyquiz/onlyquiz.component';
     QuizComponent,
     QuestComponent,
     ContactUsComponent,
-    OnlyquizComponent
+    OnlyquizComponent,
+    PictureViewerComponent,
+    SubscribeComponent,
   ],
   imports: [
     FormsModule,
