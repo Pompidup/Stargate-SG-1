a96a8c63f9808dcc97b7bf08f588c19e58b3fa6c
Pompidup <julienauvray28@gmail.com>
2019-04-30 11:59:54 +0200

    add api data on quiz and generate auto question

--- Stargate/src/app/quiz/quiz.component.ts
+++ Stargate/src/app/quiz/quiz.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
-import { quest1 } from '../nasaConst';
 import { nasa } from '../nasaClassInfo';
-import { NgForm } from '@angular/forms';
+import { quest1 } from '../nasaConst';
+
 
 
 @Component({
@@ -13,40 +12,15 @@ import { NgForm } from '@angular/forms';
 export class QuizComponent implements OnInit {
 
   public firstQuest: nasa[] = quest1;
-  public q1: string = "Title of picture ?";
-  public answerQ1: string[] = [
-    this.firstQuest[0].title,
-    "title1",
-    "title2",
-    "title3",
-  ];
 
+  constructor() {
 
-  public q2: string = "Nasa Identification ?"
-  public answerQ2: string[] = [
-    "00110100111010",
-    "La réponse est idNasa",
-    this.firstQuest[0].idNasa,
-    "Yorann",
-  ];
-  public q3: string = "Stargate secret Identification ?"
-  public answerQ3: string[] = [
-    "C'est un secret",
-    this.firstQuest[0].idMission,
-    "Bonjour demandez mot ?",
-  ];
-
-  onFormSubmit(form: NgForm) {
   }
 
-constructor() {
-  console.log(this.answerQ1[0])
-}
-
-ngOnInit() {
+  ngOnInit() {
 
 
-}
+  }
 
 }
 
