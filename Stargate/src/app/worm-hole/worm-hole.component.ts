2072623269668298b6dd109af90c9d27c5e945e9
Loup BINA <binaloup@gmail.com>
2019-04-30 17:22:59 +0200

    add all files from WholeComponent

--- Stargate/src/app/worm-hole/worm-hole.component.ts
+++ Stargate/src/app/worm-hole/worm-hole.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tabWorm } from '../wormVideo';
 
 @Component({
   selector: 'app-worm-hole',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WormHoleComponent implements OnInit {
 
+  random() {
+    let tmp: number = Math.floor(Math.random() * Math.floor(tabWorm.length -1));
+    console.log(tmp);
+    console.log(tabWorm[tmp].link);
+    let tmp2 = tabWorm[tmp].link;
+  }
+  
+
   constructor() { }
 
   ngOnInit() {
+    this.random()
   }
 
 }
